338a8c0700b9474909d9ded497c201ddfb2a9cbf
stolarchukboris <kotkotovic07@gmail.com>
2025-05-02 11:54:11 +0300

    database stuff

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import { knex } from 'knex';
+import knex from 'knex/types';
 
 const app = express();
 const PORT = 3000;
+const knex = knex({
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    }
+});
 
 app.use(express.json());
 
