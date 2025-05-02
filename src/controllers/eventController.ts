f9f93c80dd9bb124ef522688f7219a36efe70099
stolarchukboris <kotkotovic07@gmail.com>
2025-05-02 13:42:00 +0300

    typescript

---
+++ src/controllers/eventController.ts
@@ -0,0 +1,20 @@
+import app from "../index.js";
+import { Request, Response } from "express";
+
+export async function getEvents(req: Request, res: Response) {
+    try {
+        const events = await app.database('communityEvents')
+            .select('*');
+
+        if (!events) {
+            res.status(404).json({ error: 'Not found.' });
+            return;
+        }
+
+        res.status(200).json(events);
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Express } from 'express';
+import { config } from 'dotenv';
+import knex, { Knex } from 'knex';
+import eventRouter from './routes/communityEvents.js';
+
+class App {
+    database: Knex;
+    env = config().parsed || {};
+    app: Express;
+    port = 3000;
+
+    constructor() {
+        console.log('Initializing the API...');
+
+        this.app = express();
+
+        console.log('Connecting to database...');
+
+        this.database = knex({
+            client: 'mysql2',
+            connection: {
+                host: this.env.DB_HOST,
+                port: parseInt(this.env.DB_PORT, 10),
+                user: this.env.DB_USER,
+                password: this.env.DB_PASS,
+                database: this.env.DB_NAME
+            }
+        });
+
+        console.log('Starting the server...');
+
+        this.app.use(express.json());
+        this.app.use('/api/communityEvents', eventRouter);
+        this.app.listen(this.port, () => console.log(`Server running on http://localhost:${this.port}`));
+    }
+}
+
+const app = new App();
+
+export default app;
